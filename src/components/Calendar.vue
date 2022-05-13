f5329ccb89b5acb00d5e7acdb81af99454726a13
FedoTT <urkytsk3@gmail.com>
2022-05-13 13:20:32 +0800

    add-selectMonthAndYear

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,20 +1,33 @@
 <template>
 
 
-
     <div class="card">
 
         <div class="choose-section flex">
-            <div class="choose-section__month">
-                <button class="choose-btn"><i class="pi pi-chevron-left"></i></button>
+            <div class="choose-section__month flex">
+                <button
+                        class="choose-btn"
+                        @click="prevMonth">
+                    <i class="pi pi-chevron-left"/>
+                </button>
+                <span class="uppercase text-xl text-center font-bold choose-section__month-text align-items-center justify-content-center">
+                    {{ getMonthName() }}
+                </span>
+                <button class="choose-btn" @click="nextMonth"><i class="pi pi-chevron-right"></i></button>
             </div>
-            <div class="choose-section__year"></div>
+            <div class="choose-section__year flex">
+                <button @click="prevYear" class="choose-btn"><i class="pi pi-chevron-left"></i></button>
+                <span class="text-xl text-center font-bold choose-section__month-text align-items-center justify-content-center">
+                    {{ getYear() }}
+                </span>
+                <button @click="nextYear" class="choose-btn"><i class="pi pi-chevron-right"></i></button>
+            </div>
         </div>
 
         <div class="flex flex-wrap align-items-center">
             <p v-for="i in dayWeek" class="calendar-item text-center">{{ i }}</p>
             <div class="calendar-item text-center"
-                 v-for="i in days">{{i}}
+                 v-for="i in daysMonth">{{i}}
             </div>
         </div>
     </div>
@@ -28,48 +41,64 @@
     export default {
         data() {
             return {
-                days: [],
+                currentDate: new Date(),
+                daysMonth: [],
                 dayWeek: ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']
             }
         },
-
-        computed: {
-            // getDaysOfMonth() {
-            //     return new Date(year, month + 1, 0).getDate()
-            // },
-            // getFirsWeekDay(){
-            //     return new Date(year, month + 1, 1).getUTCDay()
-            // },
-            // getFirstDayInWeek(){
-            //     for(let i = this.getFirsWeekDay; i < this.getDaysOfMonth + this.getFirsWeekDay; i++){
-            //         this.days[i] = i +  1 - this.getFirsWeekDay
-            //     }
-            // }
-
-
-        },
         mounted() {
-           this.getFirstDayInWeek()
-
-            console.log('компонент смонтирован')
+            this.getFirstDayInWeek()
+        },
+        watch: {
+            currentDate(){
+                this.getFirstDayInWeek()
+            }
         },
         methods: {
             getFirstDayInWeek() {
-                const currentDate = new Date()
-                const month = currentDate.getMonth()
-                const year = currentDate.getFullYear()
+                const month = this.currentDate.getMonth()
+                const year = this.currentDate.getFullYear()
                 const getDaysOfMonth = new Date(year, month + 1, 0).getDate()
                 const getFirsWeekDay = new Date(year, month, 1).getUTCDay()
+                this.daysMonth = []
 
                 for (let i = getFirsWeekDay; i < getDaysOfMonth + getFirsWeekDay; i++) {
-                    this.days[i] = i + 1 - getFirsWeekDay
-                    console.log(this.days)
+                    this.daysMonth[i] = i + 1 - getFirsWeekDay
                 }
             },
 
-
-            test() {
+            getMonthName() {
+                let indexMonth = this.currentDate.getMonth()
+                const d = new Date(2020, indexMonth, 1)
+                return d.toLocaleDateString('ru-RU', {month: 'long'})
             },
+            selectMonth(diff) {
+                const date = new Date(this.currentDate)
+                date.setMonth(date.getMonth() - diff)
+                this.currentDate = date
+            },
+            prevMonth() {
+                this.selectMonth(1)
+            },
+            nextMonth() {
+                this.selectMonth(-1)
+            },
+
+            getYear() {
+                return this.currentDate.getFullYear()
+            },
+            selectYear(diff) {
+                const date = new Date(this.currentDate)
+                date.setFullYear(date.getFullYear() - diff)
+                this.currentDate = date
+            },
+            prevYear() {
+                this.selectYear(1)
+            },
+            nextYear() {
+                this.selectYear(-1)
+            },
+
 
         }
 
@@ -87,7 +116,17 @@
         padding-top: 50px;
     }
 
-    .choose-btn{
+    .choose-section__month {
+        width: 20rem;
+    }
+
+    .choose-section__month-text {
+        width: 7rem;
+    }
+
+
+    .choose-btn {
+        margin: 0 25px;
         border: none;
         background: none;
         cursor: pointer;
